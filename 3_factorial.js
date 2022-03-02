e9926f9b6217b6389b36263ec6d7315126503488
Arfanur <arfanur03@gmail.com>
2022-03-02 17:45:33 +0600

    3_factorial

---
+++ 3_factorial.js
@@ -0,0 +1,23 @@
+// 4! = 4*3*2*1
+// let factorial = 1;
+// for (let i = 4; i >= 1; i--) {
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+function factorial(i) {
+    if (i == 1) {
+        return 1;
+    }
+    // --i == i-1
+    return i * factorial(--i);
+}
+
+console.log(factorial(4));
+
+// factorial(4) = i * factorial(--i)
+                // 4 * factotial(3)
+                // 4 * 3 * factorial(2)
+                // 4 * 3 * 2 * factotial(1)
+                // 4 * 3 * 2 * 1
+                // 24
