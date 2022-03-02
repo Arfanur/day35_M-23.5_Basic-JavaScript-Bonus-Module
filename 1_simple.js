294fadf3047f504f7cbf297653994e3337cb6e2b
Arfanur <arfanur03@gmail.com>
2022-03-02 10:51:24 +0600

    1_simple

--- 1_simple.js
+++ 1_simple.js
@@ -1,4 +1,11 @@
-// check
-// check 2
-// check 3
-// check 4
+// for (let i = 1; i < 6; i++) {
+// console.log(i);
+// }
+function consoleNumber(i) {
+    if (i > 5) {
+        return;
+    }
+    console.log(i);
+    consoleNumber(i + 1);
+}
+consoleNumber(1);
